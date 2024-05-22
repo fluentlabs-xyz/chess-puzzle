ee11ce6e39febcb741bb4d3289410614e886ddf1
d1r1 <me@d1r1.me>
2024-05-22 12:37:37 +0400

    refactor fluent contract

--- fluent-contract/deployer/deploy-contract.js
+++ fluent-contract/deployer/deploy-contract.js
@@ -1,134 +1,169 @@
-const { Web3, ETH_DATA_FORMAT } = require("web3");
+const {Web3, ETH_DATA_FORMAT} = require("web3");
 const fs = require("fs");
 
 const DEPLOYER_PRIVATE_KEY =
-  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 const abi = [
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "board",
-        type: "string",
-      },
-      {
-        internalType: "string",
-        name: "move",
-        type: "string",
-      },
-    ],
-    name: "isCheckmate",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
+    {
+        inputs: [
+            {
+                internalType: "string",
+                name: "board",
+                type: "string",
+            },
+            {
+                internalType: "string",
+                name: "move",
+                type: "string",
+            },
+        ],
+        name: "isCheckmate",
+        outputs: [
+            {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
 ];
 
 const main = async () => {
-  if (process.argv.length < 3) {
-    console.log(`You must specify path to the WASM binary!`);
-    console.log(`Example: node deploy-contract.js --dev ./bin/greeting.wasm`);
-    process.exit(-1);
-  }
-  let args = process.argv.slice(2);
-  const checkFlag = (param) => {
-    let indexOf = args.indexOf(param);
-    if (indexOf < 0) {
-      return false;
+    if (process.argv.length < 3) {
+        console.log(`You must specify path to the WASM binary!`);
+        console.log(`Example: node deploy-contract.js --dev ./bin/greeting.wasm`);
+        process.exit(-1);
     }
-    args.splice(indexOf, 1);
-    return true;
-  };
-  let isLocal = checkFlag("--local");
-  let isDev = checkFlag("--dev");
+    let args = process.argv.slice(2);
+    const checkFlag = (param) => {
+        let indexOf = args.indexOf(param);
+        if (indexOf < 0) {
+            return false;
+        }
+        args.splice(indexOf, 1);
+        return true;
+    };
+    let isLocal = checkFlag("--local");
+    let isDev = checkFlag("--dev");
 
-  let web3Url = "";
-  if (isLocal) {
-    web3Url = "http://127.0.0.1:8545";
-  } else if (isDev) {
-    web3Url = "https://rpc.dev.thefluent.xyz/";
-  } else {
-    console.log(`You must specify --dev or --local flag!`);
-    console.log(`Example: node deploy-contract.js --dev ./bin/greeting.wasm`);
-    process.exit(-1);
-  }
+    let web3Url = "";
+    if (isLocal) {
+        web3Url = "http://127.0.0.1:8545";
+    } else if (isDev) {
+        web3Url = "https://rpc.dev.thefluent.xyz/";
+    } else {
+        console.log(`You must specify --dev or --local flag!`);
+        console.log(`Example: node deploy-contract.js --dev ./bin/greeting.wasm`);
+        process.exit(-1);
+    }
 
-  let [binaryPath] = args;
-  let wasmBinary = fs.readFileSync(binaryPath).toString("hex");
-  const web3 = new Web3(web3Url);
-  let privateKey = process.env.DEPLOYER_PRIVATE_KEY || DEPLOYER_PRIVATE_KEY;
-  let account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
+    let [binaryPath] = args;
+    let wasmBinary = fs.readFileSync(binaryPath).toString("hex");
+    const web3 = new Web3(web3Url);
+    let privateKey = process.env.DEPLOYER_PRIVATE_KEY || DEPLOYER_PRIVATE_KEY;
+    let account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
 
-  console.log("Signing transaction...");
-  const gasPrice = await web3.eth.getGasPrice(ETH_DATA_FORMAT);
-  const signedTransaction = await web3.eth.accounts.signTransaction(
-    {
-      data: "0x" + wasmBinary,
-      gasPrice,
-      gas: 300_000_000,
-      from: account.address,
-    },
-    privateKey
-  );
+    console.log("Signing transaction...");
+    const gasPrice = await web3.eth.getGasPrice(ETH_DATA_FORMAT);
+    const signedTransaction = await web3.eth.accounts.signTransaction(
+        {
+            data: "0x" + wasmBinary,
+            gasPrice,
+            gas: 300_000_000,
+            from: account.address,
+        },
+        privateKey
+    );
 
-  let contractAddress = "";
-  console.log("Sending transaction...");
-  await web3.eth
-    .sendSignedTransaction(signedTransaction.rawTransaction)
-    .on("confirmation", (confirmation) => {
-      contractAddress = confirmation.receipt.contractAddress;
-      console.log(confirmation);
-      if (contractAddress) {
-        console.log(`Contract address is: ${contractAddress}`);
-      }
+    let contractAddress = "";
+    console.log("Sending transaction...");
+    try {
+        await deployContract(web3, web3Url, signedTransaction);
+    } catch (e) {
+        console.error("Error deploying contract:", e);
+        process.exit(-1);
+    }
+    // await web3.eth
+    //   .sendSignedTransaction(signedTransaction.rawTransaction)
+    //   .on("confirmation", (confirmation) => {
+    //     contractAddress = confirmation.receipt.contractAddress;
+    //     console.log(confirmation);
+    //     if (contractAddress) {
+    //       console.log(`Contract address is: ${contractAddress}`);
+    //     }
+    //   });
+
+    const chessContract = new web3.eth.Contract(abi);
+    const functionName = "isCheckmate";
+    const board =
+        "r1bqkbnr/ppp2ppp/2np4/4p2Q/2B1P3/8/PPPP1PPP/RNB1K1NR w KQkq - 0 4";
+    const move = "Qxf7";
+
+    const functionParams = [board, move];
+    const functionData = chessContract.methods[functionName](
+        ...functionParams
+    ).encodeABI();
+
+    console.log(`Function data: ${functionData}`);
+
+    const result = await web3.eth.call({
+        to: contractAddress,
+        data: functionData,
     });
 
-  const chessContract = new web3.eth.Contract(abi);
-  const functionName = "isCheckmate";
-  const board =
-    "r1bqkbnr/ppp2ppp/2np4/4p2Q/2B1P3/8/PPPP1PPP/RNB1K1NR w KQkq - 0 4";
-  const move = "Qxf7";
+    function isASCII(str) {
+        return /^[\x00-\x7F]*$/.test(str);
+    }
 
-  const functionParams = [board, move];
-  const functionData = chessContract.methods[functionName](
-    ...functionParams
-  ).encodeABI();
+    if (isASCII(web3.utils.hexToAscii(result))) {
+        console.log(`Message: "${web3.utils.hexToAscii(result)}"`);
+    } else {
+        console.log(`Message: "${result}"`);
+    }
 
-  console.log(`Function data: ${functionData}`);
 
-  const result = await web3.eth.call({
-    to: contractAddress,
-    data: functionData,
-  });
+    const latestMinedBlockNumber = await web3.eth.getBlockNumber();
+    console.log(`Latest block number: ${latestMinedBlockNumber}`);
 
-  function isASCII(str) {
-    return /^[\x00-\x7F]*$/.test(str);
-  }
-
-  if (isASCII(web3.utils.hexToAscii(result))) {
-    console.log(`Message: "${web3.utils.hexToAscii(result)}"`);
-  } else {
-    console.log(`Message: "${result}"`);
-  }
-
-  // const signedTransaction1 = await web3.eth.accounts.signTransaction({
-  //     to: contractAddress,
-  //     gas: 1_000_000,
-  // }, DEPLOYER_PRIVATE_KEY)
-  // const receipt1 = await web3.eth.sendSignedTransaction(signedTransaction1.rawTransaction);
-  // console.log(`Receipt: ${JSON.stringify(receipt1, null, 2)}`)
-
-  const latestMinedBlockNumber = await web3.eth.getBlockNumber();
-  console.log(`Latest block number: ${latestMinedBlockNumber}`);
-
-  process.exit(0);
+    process.exit(0);
 };
+
+async function deployContract(web3, web3Url, signedTransaction) {
+    const sendResult = await fetch(web3Url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            jsonrpc: "2.0",
+            method: "eth_sendRawTransaction",
+            params: [signedTransaction.rawTransaction],
+            id: 1,
+        }),
+    });
+
+    if (!sendResult.ok) {
+        console.error("HTTP error:", sendResult.status, sendResult.statusText);
+        const errorBody = await sendResult.text();
+        console.error("Response body:", errorBody);
+        throw new Error(
+            `HTTP error ${sendResult.status}: ${sendResult.statusText}`
+        );
+    }
+
+    const sendResultJson = await sendResult.json();
+
+    if (sendResultJson.error) {
+        console.error("Error in JSON response:", sendResultJson.error);
+        throw new Error(sendResultJson.error.message);
+    }
+
+    console.log("Transaction sent, waiting for receipt...");
+    const receipt = await web3.eth.getTransactionReceipt(sendResultJson.result);
+    console.log("Receipt:", receipt);
+}
 
 main().then(console.log).catch(console.error);
--- fluent-contract/src/chess.rs
+++ fluent-contract/src/chess.rs
@@ -1,48 +1,47 @@
-use alloy_sol_types::{sol, SolCall, SolType, SolValue};
-use fluentbase_sdk::{ContextReader, ExecutionContext, LowLevelAPI, LowLevelSDK};
-use shakmaty::{fen::Fen, san::San, CastlingMode, Chess, FromSetup, Position, Setup};
-
 use alloc::string::String;
 
+use alloy_sol_types::{sol, SolCall, SolValue};
+use fluentbase_sdk::{ContextReader, ExecutionContext, LowLevelAPI, LowLevelSDK};
+use shakmaty::{CastlingMode, Chess, fen::Fen, FromSetup, Position, san::San, Setup};
+
+// Define a Solidity function that checks if a given chess move results in a checkmate
 sol! {
     function isCheckmate(string board, string mv) external view returns (bool);
 }
 
-macro_rules! forward_evm_call {
-    ($func_type:ty, $input:expr, $self:ident, $fn_name:ident, 2) => {{
-        let input =
-            <<$func_type as SolCall>::Parameters<'_> as SolType>::abi_decode(&$input[4..], false)
-                .unwrap();
-        let output = $self.$fn_name(input.0, input.1);
-        output.abi_encode()
-    }};
-}
-
+// Define a struct to hold the execution context
 struct CHESS<'a>(&'a mut ExecutionContext);
 
 impl<'a> CHESS<'a> {
+    // Function to check if a given chess move results in a checkmate
     fn is_checkmate(board: &str, mv: &str) -> bool {
+        // Parse the FEN string to a Fen object
         let fen = match Fen::from_ascii(board.as_bytes()) {
             Ok(fen) => fen,
             Err(_) => return false,
         };
 
+        // Convert the Fen object to a Setup object
         let setup = Setup::from(fen);
 
+        // Convert the Setup object to a Chess object
         let pos = match Chess::from_setup(setup, CastlingMode::Standard) {
             Ok(pos) => pos,
             Err(_) => return false,
         };
+        // Parse the move string to a San object
         let san = match mv.parse::<San>() {
             Ok(san) => san,
             Err(_) => return false,
         };
 
+        // Convert the San object to a Move object
         let mv = match san.to_move(&pos) {
             Ok(mv) => mv,
             Err(_) => return false,
         };
 
+        // Try to play the move on the chess board
         match pos.play(&mv) {
             Ok(_) => true,
             Err(_) => false,
@@ -50,32 +49,32 @@ impl<'a> CHESS<'a> {
     }
 }
 
+// Function to deploy the contract
 pub fn deploy() {}
 
+// Main function
 pub fn main() {
-    let mut ctx = ExecutionContext::default();
+    // Create a default execution context
+    let ctx = ExecutionContext::default();
+    // Get the contract input
     let input = ctx.contract_input().clone();
+    // Initialize the selector
     let mut selector: [u8; 4] = [0; 4];
     selector.copy_from_slice(&input[0..4]);
-    let chess_handler = CHESS(&mut ctx);
 
+    // Decode the contract input
     let (board, mv): (String, String) = match <isCheckmateCall as SolCall>::abi_decode(&input, true)
     {
         Ok(decoded) => (decoded.board, decoded.mv),
         Err(e) => {
-            panic!("Failed to decode input bytes: {:?}", e);
+            panic!("Failed to decode input {:?}", e);
         }
     };
-
-    // let result = CHESS::<'_>::is_checkmate(&board, &mv);
-    // if result {
-    //     LowLevelSDK::sys_write("Success".as_bytes());
-    // } else {
-    //     LowLevelSDK::sys_write("Failed".as_bytes());
-    // }
-    LowLevelSDK::sys_write(&board.abi_encode());
-
-    // // TODO: get func selector (first 4 bytes)
-    // // TODO: impl router for function calls
-    // // TODO: return result
+    // Execute the appropriate function based on the selector
+    let output = match selector {
+        isCheckmateCall::SELECTOR => CHESS::<'_>::is_checkmate(&board, &mv).abi_encode(),
+        _ => panic!("unknown method"),
+    };
+    // Write the output to the system
+    LowLevelSDK::sys_write(&output);
 }
